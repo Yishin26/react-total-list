5ae647e5f9fa7339b5a28287ea142d800d401e3d
Louise <yishinlin@outlook.com>
2023-02-15 22:07:30 +0800

    試用：bootstrap navbar, position

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import logo from './assets/logo.svg';
-import './assets/App.css';
-import Input from './components/input.js'
 import './assets/all.scss'
 
 function App() {
-  const [text, setText] = useState('')
-
-  const onChangeHandler = e => {
-    setText(e.target.value)
-  }
-
-  useEffect(() => {
-
-    (async () => {
-      const result=await axios.get('https://randomuser.me/api')
-      console.log(result)
-    }
-    )()
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-
-        {text}
-        <Input id={'sample'} text='這是Input' value={text} onChangeHandler={onChangeHandler} />
-        <br/>
-        <button type="button" className="btn btn-primary">Primary</button>
-      </header>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand">Shop</span>
+        <button class="btn btn-outline-dark my-2 my-sm-0 position-relative" type="submit">購物車<span className="badge position-absolute top-0 start-100 translate-middle">10</span></button>
+      </nav>
     </div>
   );
 }
